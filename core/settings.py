80ee7b3dc45ec88169136eb9e5cbe3428ffb7676
musk96 <musk96@hotmail.fr>
2022-08-27 15:29:32 +0200

    added angular config

--- core/settings.py
+++ core/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default = 'quizappsecretkey')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get('DEBUG', default = '1'))
 
-ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOST')]
+ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOST', default='127.0.0.1')]
 
 
 # Application definition
